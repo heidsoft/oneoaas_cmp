08e6a275ebc792deb0814d454303315ee28cf7b5
heidsoft <heidsoft@qq.com>
2017-08-15 15:18:08 +0800

    Update 发布时，删除py文件，保留pyc

--- build/release.sh
+++ build/release.sh
@@ -4,3 +4,19 @@
 APP_ROOT=$(dirname "${BASH_SOURCE}")/..
 
 echo ${APP_ROOT}
+
+cd ${APP_ROOT}
+
+rm -rf home_application/*.py
+rm -rf home_application/aliyun/*.py
+rm -rf home_application/aws/*.py
+rm -rf home_application/migrations/*.py
+rm -rf home_application/overview/*.py
+rm -rf home_application/qcloud/*.py
+rm -rf home_application/ucloud/*.py
+rm -rf home_application/vmware/*.py
+rm -rf hybirdsdk/*.py
+rm -rf hybirdsdk/*.py
+
+
+
