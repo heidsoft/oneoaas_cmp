069c1023b5aab23fa2f45b1971587857ad5a0881
heidsoft <heidsoft@qq.com>
2017-08-15 15:53:03 +0800

    Update 更新打包自动化文件

--- build/release.sh
+++ build/release.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+echo "release start ..."
+
 #定义应用根目录
+APP_NAME="oneoaas-cmp"
+APP_PKG=${APP_NAME}.tar.gz
 APP_ROOT=$(dirname "${BASH_SOURCE}")/..
-
-echo ${APP_ROOT}
-
+RELEASE_ROOT=release/oneoaas-cmp
+APP_PUBLIC_DIR="account app_control blueking common conf error_pages home_application hybirdsdk  QcloudApi static templates wssh"
+APP_PUBLIC_FILE="__init__.py manage.py requirements.txt settings.py urls.py wsgi.py"
 cd ${APP_ROOT}
+
+
+echo "delete some py file "
 
 rm -rf home_application/*.py
 rm -rf home_application/aliyun/*.py
@@ -18,5 +25,21 @@ rm -rf home_application/vmware/*.py
 rm -rf hybirdsdk/*.py
 rm -rf hybirdsdk/*.py
 
+echo "create release dir "
+
+mkdir -p ${RELEASE_ROOT}
+mkdir -p ${RELEASE_ROOT}/src
+
+cp -rf APP_PUBLIC_DIR ${RELEASE_ROOT}/src
+
+cp -rf APP_PUBLIC_FILE ${RELEASE_ROOT}/src
+
+cp -rf app.yml ${RELEASE_ROOT}/src
+
+cp -rf pkgs ${RELEASE_ROOT}
+
+tar zcf ${APP_PKG}  ${RELEASE_ROOT}
+
+echo "release finish ..."
 
 
